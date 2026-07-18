c05a3fa3f3bc3045a3100262ef110cc0a29c58d4
Joljolionn <joljolionn@gmail.com>
2026-07-18 03:37:29 -0300

    feat: adicionar criador de polinomios geradores

--- new_app.js
+++ new_app.js
@@ -22,3 +22,44 @@ for (let I = 0; I < 255; I++) {
 }
 ANTILOG[255] = ANTILOG[0];
 
+// [FUNÇÕES AUXILIARES]
+// Primeiro, cria-se o polinômio para 3 correction codewords
+// Depois, criam-se os próximos usando os termos até n-1
+function createGenPolynomial(n) {
+    let polinomio = [1]; // primeiro termo (1x^0 ou 1)
+
+    for (let I = 0; I < n; I++) {
+        let alpha = ANTILOG[I]; // acha o termo de alpha nessa iteração
+        let novoTermo = [1, alpha]; // cria o novo termo (x - a^n)
+
+        // Usa a regra de polinômios em que, dado a multiplicação de dois
+        // polinômios, o grau do polinômio resultante será igual à soma dos maiores
+        // coeficientes de cada polinômio -1
+        let resultadoLen = polinomio.length + novoTermo.length - 1;
+        let resultado = new Array(resultadoLen).fill(0);
+
+        // loop duplo para multiplicar termos dos dois polinomios indvidualmente
+        for (let iPolinomio = 0; iPolinomio < polinomio.length; iPolinomio++) {
+            for (let iNovo = 0; iNovo < novoTermo.length; iNovo++) {
+                // busca os expoentes de cada alpha
+                let coeficientePolinomio = polinomio[iPolinomio]
+                let coeficienteNovo = novoTermo[iNovo]
+
+                if (coeficientePolinomio == 0 || coeficienteNovo == 0) {
+                    continue;
+                }
+                let expoentePolinomio = LOG[coeficientePolinomio];
+                let expoenteNovo = LOG[coeficienteNovo];
+
+                // regras para multiplicação dos expoentes no Campo de Galois
+                    // devolve o valor inteiro após adição de expoentes e
+                    // modulação por 255 para garantir que o número esteja dentro
+                    // do campo e faz a adição com xor
+                    resultado[iPolinomio + iNovo] ^= ANTILOG[((expoenteNovo + expoentePolinomio) % 255)];
+            }
+        }
+      polinomio = resultado
+    }
+  return polinomio
+}
+
